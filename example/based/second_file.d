2d7b5d59180a221b55b1957c2752866d0951cfb0
Graham St Jack <graham.stjack@internode.on.net>
2013-07-18 18:27:52 +0930

    Small extesion to the example to demonstrate an exe with multiple source files.

---
+++ example/based/second_file.d
@@ -0,0 +1,7 @@
+module based.second_file;
+
+// A second file to link into test-one to show that an exe can be built with multiple source files.
+
+int add_stuff(int one, int two) {
+    return one + two;
+}
--- example/based/test_one.d
+++ example/based/test_one.d
@@ -1,8 +1,10 @@
 import based.one;
+import based.second_file;
 
 import std.stdio;
 
 void main() {
     double x = 5.0;
     assert(square(x) == x * x, "unexpected answer");
+    add_stuff(1, 2);
 }
